bbd0fdf1bfb9ef9cb27d479fdda8051a2f04668a
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-11 22:11:34 +0300

    added all entityframework in dal

---
+++ DataAccessLayer/EntityFramework/EfContactRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfContactRepository : GenericRepository<Contact>, IContactDal
+    {
+    }
+}
