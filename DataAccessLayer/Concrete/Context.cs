3880d6929d94ccf2ea1a7884240f04d473e4de25
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-11 15:43:22 +0300

    added context class for database connection

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-2H5V0KB\\SQLEXPRESS;database=DbBlog;integrated security=true;");
+        }
+    }
+}
