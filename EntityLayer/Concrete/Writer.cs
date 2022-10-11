8280dacf80b0690e619de711d7529727eaa3d314
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-11 15:07:12 +0300

    added writer entity

---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        public int WriterID { get; set; }
+        public string WriterName { get; set; }
+        public string WriterAbout { get; set; }
+        public string WriterImage { get; set; }
+        public string WriterStatus { get; set; }
+    }
+}
