b8eb48caf0140de57cfc6eeee87ce25e361ab35b
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-11 15:10:39 +0300

    added comment entity

---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public string CommentUserName { get; set; }
+        public string CommentTitle { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime CommentDate { get; set; }
+    }
+}
