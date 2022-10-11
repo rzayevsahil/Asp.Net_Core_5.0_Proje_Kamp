4fa71f99efdd0b9845020deaa68ec2669bdb8aeb
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-11 15:02:08 +0300

    added acticle entity

---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        public int BlokID { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogThumbnailImage { get; set; }
+        public string BlogImage { get; set; }
+        public string CreateDate { get; set; }
+        public bool BlogStatus { get; set; }
+    }
+}
