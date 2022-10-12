dd95e3c4b2de1cb1a05106f00c87e31b6e663dd1
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-12 17:20:46 +0300

    added categoru controller

---
+++ UILayer/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var values = categoryManager.TGetList();
+            return View(values);
+        }
+    }
+}
