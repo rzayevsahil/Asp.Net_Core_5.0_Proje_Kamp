a9ace112da45b5515fcbb766173d2f46f241c459
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-12 20:21:03 +0300

    added partial1

---
+++ UILayer/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
+    }
+}
