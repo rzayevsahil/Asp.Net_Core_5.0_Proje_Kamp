492efd9eb031601f89894567ef1d54e8eaf5e463
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-21 16:56:33 +0300

    added viewcomponents

---
+++ UILayer/ViewComponents/CommentList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
