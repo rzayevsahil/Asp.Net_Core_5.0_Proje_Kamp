3209e4e88810024fa29b1f2730a0214a45579651
Sahil Rzayev <sahilrzayev200d@gmail.com>
2022-10-21 22:36:10 +0300

    updated

---
+++ UILayer/ViewComponents/Category/CategoryList.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.ViewComponents.Category
+{
+    public class CategoryList : ViewComponent
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var blogCount = categoryManager.TGetList().Select(x=>x.Blogs.Count());
+            ViewBag.blogCount = blogCount;
+            var values = categoryManager.TGetList();
+            return View(values);
+        }
+    }
+}
